03d27474a965cf8a957f384c5bcf12b420ebd507
Muaz Khan <muazkh@gmail.com>
2014-10-31 18:03:29 +0500

    MediaRecorder.js updated ( MediaRecorderWrapper )
    https://github.com/streamproc/MediaStreamRecorder/blob/master/AudioStreamRecorder/MediaRecorder.js
    
    Fixed (in Firefox):
    https://www.webrtc-experiment.com/msr/video-recorder.html

--- old/v1.1/FileSystem/FileReader.js
+++
@@ -1,7 +0,0 @@
-// Muaz Khan     - https://github.com/muaz-khan 
-// neizerth      - https://github.com/neizerth
-// MIT License   - https://www.webrtc-experiment.com/licence/
-// Documentation - https://github.com/streamproc/MediaStreamRecorder
-// ==========================================================
-// FileReader.js
-// implementation needed
